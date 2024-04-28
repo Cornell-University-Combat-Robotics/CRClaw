73665b2cf07091128b23081cf692d73b0049d530
halyssa <aahsu081405@gmail.com>
2024-04-28 13:17:26 -0400

    edited limit class

--- switch_test.py
+++ switch_test.py
@@ -1,8 +1,17 @@
 from switch import Limit
 
+# l = Limit(16)
+# try:
+#     while(True):
+#         print(l.isPressed())
+# except: 
+#     l.cleanup()
 l = Limit(16)
+l.button_callback()
+l.add_event()
 try:
     while(True):
-        print(l.isPressed())
-except: 
+        l.is_touched()
+except:
     l.cleanup()
+

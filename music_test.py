4f17d8b03225a327de8f2d0ced8e73f31e70d288
grace <gl487@cornell.edu>
2025-01-25 15:07:39 -0500

    groot + readme

--- music_test.py
+++ music_test.py
@@ -1,16 +1,29 @@
+import pygame
 import os
 
-import pygame
-
 pygame.mixer.init()
+path = os.getcwd() + "/groot_sounds"
+sound_files = ["I_am_groot_1.mp3", 
+               "I_am_groot_2.mp3", 
+               "I_am_groot_3.mp3", 
+               "I_am_groot_4.mp3"]
+pygame.mixer.music.set_volume(1)
 
-if os.path.exists("test_music.wav"):
-    pygame.mixer.music.load("test_music.wav")
+for sound_file in sound_files:
+    pygame.mixer.music.load(path + sound_file)
     pygame.mixer.music.play()
-
+    
     while pygame.mixer.music.get_busy():
-        print("playing")
+    	continue
+        
+# if os.path.exists("test_music.wav"):
+#     pygame.mixer.music.load("test_music.wav")
+#     pygame.mixer.music.play()
 
-    pygame.mixer.quit()
-else:
-    print("Error: test_music.wav not found")
+#     while pygame.mixer.music.get_busy():
+#         print("playing")
+
+#     pygame.mixer.quit()
+# else:
+#     print("Error: test_music.wav not found")
+

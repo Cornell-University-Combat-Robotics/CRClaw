17cae0813fc46b46dddb160a4cf0c70ceb356c47
grace <gl487@cornell.edu>
2025-02-01 12:00:20 -0500

    pixaby

--- music_test.py
+++ music_test.py
@@ -1,21 +1,19 @@
-import pygame
 import os
 
+import pygame
+
 pygame.mixer.init()
-path = os.getcwd() + "/groot_sounds"
-sound_files = ["I_am_groot_1.mp3", 
-               "I_am_groot_2.mp3", 
-               "I_am_groot_3.mp3", 
-               "I_am_groot_4.mp3"]
+path = os.getcwd()
+sound_files = ["pixaby_audio.mp3"]
 pygame.mixer.music.set_volume(1)
 
 for sound_file in sound_files:
     pygame.mixer.music.load(path + sound_file)
     pygame.mixer.music.play()
-    
+
     while pygame.mixer.music.get_busy():
-    	continue
-        
+        continue
+
 # if os.path.exists("test_music.wav"):
 #     pygame.mixer.music.load("test_music.wav")
 #     pygame.mixer.music.play()
@@ -26,4 +24,3 @@ for sound_file in sound_files:
 #     pygame.mixer.quit()
 # else:
 #     print("Error: test_music.wav not found")
-

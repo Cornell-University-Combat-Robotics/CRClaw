d6b4aeffe689ef173e9fe0af22ed1371faaff770
TVijay2021 <tejusvi.vijay@gmail.com>
2026-03-07 15:33:47 -0500

    Working test message code for LCD

--- lcd/lcd_test.py
+++ lcd/lcd_test.py
@@ -1,29 +1,57 @@
 from lcd_i2c import LCD
 from machine import I2C, Pin
-import time
+from time import sleep, ticks_ms, ticks_diff
 
-lcd_startmsg = "Insert 1 Coin to Start"
-lcd_playmsg = "The Game has Started"
-lcd_successmsg = "You are Successful!"
-lcd_failmsg = "You Failed. Try again with Another Coin"
+# ---- SET UP ------
+I2C_ADDR = 0x27
+I2C_NUM_ROWS = 4
+I2C_NUM_COLS = 20
+FREQ = 800000
 
-
-i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=800000)
-lcd = LCD(addr=0x27, cols=4, rows=20, i2c=i2c)
+i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=FREQ)
+lcd = LCD(addr=I2C_ADDR, cols=I2C_NUM_COLS, rows=I2C_NUM_ROWS, i2c=i2c)
+lcd.begin()
 lcd.display()
-
-lcd.print(lcd_startmsg)
-time.sleep(1.0)
 lcd.clear()
 
-lcd.print(lcd_playmsg)
-time.sleep(1.0)
+# ---- MESSAGES ------
+lcd_startmsg = "Insert 1 coin to start!"
+lcd_playmsg = "The game has started!"
+lcd_successmsg = "You are successful!"
+lcd_failmsg = "You Failed. Try again with another coin."
+
+# ---- FUNCTION DEFINITIONS ------
+def scroll_message(lcd, message, row=0, duration=30, loop_delay=0.75, gap=2):
+    width = lcd.cols
+    
+    # small gap between loops
+    text = message + " " * gap
+    text_len = len(text)
+
+    start = ticks_ms()
+    pos = 0
+
+    while ticks_diff(ticks_ms(), start) < duration * 1000:
+        window = ""
+
+        for i in range(width):
+            window += text[(pos + i) % text_len]
+
+        lcd.set_cursor(0, row)
+        lcd.print(window)
+
+        pos = (pos + 1) % text_len
+        sleep(loop_delay)
+
+# ---- TEST CODE ------
+scroll_message(lcd, lcd_startmsg, row=0, duration=10)
 lcd.clear()
 
-lcd.print(lcd_successmsg)
-time.sleep(1.0)
+scroll_message(lcd, lcd_playmsg, row=0, duration=10)
 lcd.clear()
 
-lcd.print(lcd_failmsg)
-time.sleep(1.0)
+scroll_message(lcd, lcd_successmsg, row=0, duration=10)
+lcd.clear()
+
+scroll_message(lcd, lcd_failmsg, row=0, duration=10)
 lcd.clear()
